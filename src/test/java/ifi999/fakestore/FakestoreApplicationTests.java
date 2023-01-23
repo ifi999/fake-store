c4daa383edaefb9c5148c542c2d7b6aabb86b4af
ifi999 <kkwkjm4231@gmail.com>
2023-01-23 22:12:21 +0900

    add: fake store 초기설정

---
+++ src/test/java/ifi999/fakestore/FakestoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ifi999.fakestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakestoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
